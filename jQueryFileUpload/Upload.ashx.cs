42502d0d7e624fec8ab3bd004ab0f8ba1ae86aa0
Danny Miller <danny@dmiller.ca>
2011-10-13 17:25:02 -0300

    Remove unnecessary usings

--- jQueryFileUpload/Upload.ashx.cs
+++ jQueryFileUpload/Upload.ashx.cs
@@ -1,9 +1,7 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Web;
 using System.Web.Script.Serialization;
-using System.IO;
 
 namespace WebInterface.General.Handlers
 {
